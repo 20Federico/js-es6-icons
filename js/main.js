3a3f652e2448ba0d32ae8f03082f0c397a1d52e5
20Federico <fefe.molino@gmail.com>
2021-10-26 17:47:31 +0200

    added color for each type

--- js/main.js
+++ js/main.js
@@ -104,10 +104,28 @@ const cardContainer = document.querySelector('.cardContainer');
 icons.forEach(element => {
   
   cardContainer.innerHTML += `
+  <div class="col"> 
   <div class="card">
-    <i class="${element.family} ${element.prefix}${element.name}"></i>
-    <h5 class="mb-0 mt-1">${element.name}</h5>
+  <i class="${element.family} ${element.prefix}${element.name}" type="${element.type}"></i>
+  <h5 class="mb-0 mt-1">${element.name}</h5>
   </div>
-  `;
+  </div>`;
 
+});
+
+const icon = document.querySelectorAll('.col .card i');
+
+icon.forEach(icon => {
+  
+  switch (icon.getAttribute('type')) {
+    case 'animal':
+      icon.style.color = '#0000FF';
+      break;
+    case 'vegetable':
+      icon.style.color = '#FFA500';
+      break;
+    case 'user':
+      icon.style.color = '#800080';
+      break;
+      }  
 });
