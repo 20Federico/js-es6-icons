df507375fb539ac053d25687c3694496ef30374d
20Federico <fefe.molino@gmail.com>
2021-10-26 19:21:32 +0200

    added filters. ex completed

--- js/main.js
+++ js/main.js
@@ -86,23 +86,69 @@ const icons = [
 		family: 'fas'
 	},
 	{
-		name: 'user-ninja',
+    name: 'user-ninja',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas'
 	},
 	{
-		name: 'user-secret',
+    name: 'user-secret',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas'
 	}
 ];
+const animalFilter = icons.filter(icon => icon.type === 'animal')
+const vegetableFilter = icons.filter(icon => icon.type === 'vegetable')
+const userFilter = icons.filter(icon => icon.type === 'user')
 
 const cardContainer = document.querySelector('.cardContainer');
+const filterSelect = document.getElementById('iconsFilter');
 
-icons.forEach(element => {
+icons.forEach(element => createCard(element));
+let iconList = document.querySelectorAll('.col .card i');
+iconColour(iconList);
+
+filterSelect.addEventListener('change', function () {
   
+  cardContainer.innerHTML = ''
+
+  switch (filterSelect.value) {
+    case 'all':
+      icons.forEach(element => createCard(element));
+      break;
+    case 'animal':
+      animalFilter.forEach(element => createCard(element));
+      break;
+    case 'vegetable':
+      vegetableFilter.forEach(element => createCard(element));
+      break;
+    case 'user':
+      userFilter.forEach(element => createCard(element));
+      break;
+  }
+
+  iconList = document.querySelectorAll('.col .card i');
+  iconColour(iconList)
+});
+
+function iconColour(iconsArray) {
+  iconsArray.forEach(element => {
+    switch (element.getAttribute('type')) {
+      case 'animal':
+        element.style.color = '#0000FF';
+        break;
+      case 'vegetable':
+        element.style.color = '#FFA500';
+        break;
+      case 'user':
+        element.style.color = '#800080';
+        break;
+        }  
+  });
+}
+
+function createCard(element) {
   cardContainer.innerHTML += `
   <div class="col"> 
   <div class="card">
@@ -110,22 +156,4 @@ icons.forEach(element => {
   <h5 class="mb-0 mt-1">${element.name}</h5>
   </div>
   </div>`;
-
-});
-
-const icon = document.querySelectorAll('.col .card i');
-
-icon.forEach(icon => {
-  
-  switch (icon.getAttribute('type')) {
-    case 'animal':
-      icon.style.color = '#0000FF';
-      break;
-    case 'vegetable':
-      icon.style.color = '#FFA500';
-      break;
-    case 'user':
-      icon.style.color = '#800080';
-      break;
-      }  
-});
+}
